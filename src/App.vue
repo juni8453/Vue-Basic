38a549439d7cdbb926be754f8adadd8ee4ce6359
ByeoungJoon Jeon <juni8453@naver.com>
2023-12-19 14:23:16 +0900

    v-for 활용

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
 
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div>
-    <h4>XX 원룸</h4>
-    <p>{{ price1 }}만원</p>
+  <div class="menu">
+    <!-- key 는 인덱스로 자주 사용 -->
+    <a v-for="(menu,i) in menus" :key="i"> {{ menu }} </a>
   </div>
-  <div>
-    <h4>XX 원룸</h4>
-    <p>{{ price2 }}만원</p>
+
+  <div v-for="(product,i) in products" :key="i">
+    <h4>{{ product }}</h4>
+    <p>{{ prices[i] }} 만원</p>
   </div>
+
 </template>
 
 <script>
@@ -18,22 +19,36 @@ export default {
   // 데이터 바구니 data(){} 로 실시간 자동 렌더링 데이터 바인딩 가능
   data() {
     return {
-      price1: 60,
-      price2: 70,
+      products: ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
+      prices: [50, 60, 60],
+      menus: ['Home', 'Products', 'About'],
     }
   },
   components: {
   }
 }
+
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
 </style>
